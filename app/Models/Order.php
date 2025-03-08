8d3ab445eadad92dbc556658f8004ac924905fda
rasheedgavin <rasheedgavinesponga@gmail.com>
2025-03-08 17:06:54 +0800

    initial commit

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory, BelongsTo;
+
+    protected $fillable = [
+        'user_id',
+        'item',
+        'quantity'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
